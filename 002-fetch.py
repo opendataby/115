94da89e45ca1fdef777cc3b145a342784dbb15df
Anatoli Babenia <anatoli@rainforce.org>
2017-04-02 13:23:43 +0300

    С отформатированным JSON приятнее работать

--- 002-fetch.py
+++ 002-fetch.py
@@ -1,22 +1,26 @@
 
+import json
 import os
 import urllib
 
 APIURL = 'http://115.xn--90ais/api/problem/getlist'
 
 
-def escape2unicode(string):
-    """
-    Convert from \u043c\u0430\u0440 to unicode object to utf-8
-    """
-    return string.decode('unicode-escape').encode('utf-8')
+def indent(jsondata):
+    try:
+        data = json.loads(jsondata)
+    except ValueError:
+        open('.002-fetch-error', 'wb').write(jsondata.encode('utf-8'))
+        print('error loading json, data saved to .002-fetch-error.txt')
+        raise
+    return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
 def get_month_data(month, cookie, token):
     params = 'date={}&_token={}'.format(month, token)
     opener = urllib.FancyURLopener()
     opener.addheader('Cookie', cookie)
     stream = opener.open(APIURL, params)
-    return escape2unicode(stream.read())
+    return stream.read()
 
 
 if __name__ == '__main__':
@@ -30,5 +34,8 @@ if __name__ == '__main__':
         if os.path.exists(filename):
             continue
         with open(filename, 'wb') as fw:
-            fw.write(get_month_data(month, cookie, token))
+            data = get_month_data(month, cookie, token)
+            # parse, ident json data and save to string
+            data = indent(data)
+            fw.write(data.encode('utf-8'))
             print('saved %s' % filename)
