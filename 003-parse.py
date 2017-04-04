6b567e5633c74729ce381e4ff35705dd31308a3c
Anatoli Babenia <anatoli@rainforce.org>
2017-04-04 21:03:55 +0300

    Парсер закончен. Итого - три полных дня не считая прошлых подходов

--- 003-parse.py
+++ 003-parse.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """
+https://github.com/opendataby/city-dashboard/issues/53
+
+Example:
 
     "items": {
         "68036": {
@@ -32,6 +35,15 @@
         }, 
         "68037": {
 
+Possible status values:
+
+    > findstr "status" *.json | cut -d":" -f 2,3,4 | "C:\Program Files\Dev\Git\usr\bin\sort.exe" -u
+
+        "status": "1",
+        "status": "3",
+        "status": "4",
+        "status": "5",
+        "status": "7",
 """
 
 import csv
@@ -53,27 +65,62 @@ def fields(ob):
 def json2csv(inname, outname):
     data = json.load(open(inname, 'r'))
     with open(outname, 'wb') as outcsv:
-        outcsv.write('# https://github.com/opendataby/city-dashboard/issues/53\n')
-        outcsv.write('# \n')
-        names = ['id', 'category', 'author']
+        #outcsv.write('# https://github.com/opendataby/city-dashboard/issues/53\n')
+        #outcsv.write('# \n')
+        names = ['id', 'category', 'author', 'name', 'address', 'status', 'opened', 'planned']
         writer = csv.DictWriter(outcsv, fieldnames=names)
         writer.writeheader()
         for issue in data['items']:
-           item = data['items'][issue].copy()
-           
-           fields(item['category'])
-           entry = odict([
-             ('id', item['id']),
-             ('category', '{parent_id}.{id}'.format(**item['category'])),
-             ('author', item['user']['id']),
-           ])
+            item = data['items'][issue].copy()
+            
+            #fields(item['category'])
+            created = item['date_create'].split()[2]  # "1 апреля 0:16"
+            authorid = item['user']['id'] if item['user'] else 0
+            name = item['user']['name'].encode('utf-8').strip() if item['user'] else ''
+            try:
+                entry = odict([
+                  ('id', item['id']),
+                  ('category', '{parent_id}.{id}'.format(**item['category'])),
+                  ('author', authorid),
+                  ('name', name),
+                  ('address', item['address'].encode('utf-8').strip()),
+                  ('status', item['status']),
+                  ('opened', item['crm_create_at']+" "+created),
+                  ('planned', item['crm_date_planned']),
+                ])
+            except TypeError:
+                print('Error: file {}, issue {}'.format(inname, item['id']))
+                raise
 
-           writer.writerow(entry)
-           sys.exit()
+            """
+             "address": "рядом с улица Максима Богдановича, 143", 
+             "crm_create_at": "2017-04-01", 
+             "crm_date_planned": "2017-04-03", 
+             "date_create": "1 апреля 0:16", 
+             "date_planned": "03.04.2017", 
+             "lat": "53.92498779", 
+             "lng": "27.57044029", 
+             "photo": {
+                 "after": [], 
+                 "before": []
+             }, 
+             "rating": "0", 
+             "status": "3", 
+             "user": {
+                 "id": "6635", 
+                 "last_name": "Евгений", 
+                 "middle_name": "", 
+                 "name": "Евгений"
+             }
+             """
+
+            writer.writerow(entry)
+            #sys.exit()
 
 
 if __name__ == '__main__':
-    for name in ['003-in-2017-04-01.json']:# !!! [ ] os.listdir('.'):
+    #for name in ['003-in-2017-04-01.json']:
+    for name in os.listdir('.'):
         # 003-in-2017-04-01.json
         if name.startswith('003-in-') and name.endswith('.json'):
             month = name[7:-5]
