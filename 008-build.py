0d0058eed4fcb2f4fae8228800a89d9804b17d19
Anatoli Babenia <anatoli@rainforce.org>
2017-05-19 07:50:02 +0300

    Вывод index.html в две колонки - исходники и датасет

--- 008-build.py
+++ 008-build.py
@@ -5,16 +5,24 @@ import os
 
 if __name__ == '__main__':
     files = os.listdir('.')
-    fields = {'filelist': ''}
+    fields = {
+      'filelist': '',
+      'dataset': '',
+    }
     for f in files:
         if f == '.git':
             continue
-        fields['filelist'] += '<div><a href={0}>{0}</a></div>\n'.format(f)
+        appendto = 'filelist'
+        if f.startswith('opendata'):
+            appendto = 'dataset'
+        fields[appendto] += '<div><a href={0}>{0}</a></div>\n'.format(f)
 
     with open('index.html.tpl', 'rb') as template:
         html = template.read()
+        for field in fields.keys():
+            html = html.replace('{'+field+'}', fields[field])
         
         with open('index.html', 'wb') as out:
-            out.write(html.format(**fields))
+            out.write(html)
 
         print('saved index.html')
--- index.html.tpl
+++ index.html.tpl
@@ -1,5 +1,24 @@
+<style>
+body {
+  display: flex;
+  padding: 10pt;
+}
+body > div {
+  margin: 10pt;
+}
+h5 > a {
+  color: black;
+}
+</style>
+
 <body>
   <div id="files">
+    <h5>source</h5>
 {filelist}
+    <h5><a href='https://github.com/opendataby/115'>https://github.com/opendataby/115</a></h5>
+  </div>
+  <div id="dataset">
+    <h5>dataset</h5>
+{dataset}
   </div>
 </body>
