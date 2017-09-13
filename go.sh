5c1dd60f853b69238b2a2ed13f6c7aa171db6639
mrR2D2 <alaksandr.lavoncyk@gmail.com>
2017-09-13 14:35:04 +0300

    Слияние всех .csv в один и .gitignore

--- go.sh
+++ go.sh
@@ -1,5 +1,10 @@
-python 001-order.py -f
-python 002-fetch.py
-python 003-parse.py
+python2 001-order.py -f
+python2 002-fetch.py
+python2 003-parse.py
+
+head -1 $(ls opendata-115-2* | head -1) > opendata-115-all.csv
+tail -n+2 -q opendata-115-2* >> opendata-115-all.csv
+
 zip "dataset-115.bel-$(date +'%Y%m%d-%H%M%S').zip" opendata*
-python 008-build.py
+
+python2 008-build.py
